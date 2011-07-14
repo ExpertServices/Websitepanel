d104ec3116c6b762e1722adcf602f606a0cb313b
ptsurbeleu <ptsurbeleu@localhost>
2011-07-13 23:15:40 -0700

    Items being checked-in:  - Updated list of files/folders to ignore;  - Added full path to FCIV tool;  - Updated settings of WebsitePanel.LocalizationToolkit solution output for Release/Debug;  - Updated build.xml with relative paths to solutions;  - Updated versioning for WebsitePanel.Templates project;

--- WebsitePanel.Installer/Resources/Compute SHA1.bat
+++ WebsitePanel.Installer/Resources/Compute SHA1.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-fciv.exe "..\Build\debug\WebsitePanelInstaller11.msi" -sha1 > "..\Build\debug\SHA1.log"
-fciv.exe "..\Build\release\WebsitePanelInstaller11.msi" -sha1 > "..\Build\release\SHA1.log"
+"C:\Program Files (x86)\Microsoft\FCIV\fciv.exe" "..\Build\debug\WebsitePanelInstaller12.msi" -sha1 > "..\Build\debug\SHA1.log"
+"C:\Program Files (x86)\Microsoft\FCIV\fciv.exe" "..\Build\release\WebsitePanelInstaller12.msi" -sha1 > "..\Build\release\SHA1.log"
 
 PAUSE
--- WebsitePanel/Sources/VersionInfo.cs
+++ WebsitePanel/Sources/VersionInfo.cs
@@ -16,7 +16,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Outercurve Foundation")]
 [assembly: AssemblyCopyright("Copyright © 2011 Outercurve Foundation.")]
 [assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.39")]
+[assembly: AssemblyFileVersion("1.2.0.38")]
 [assembly: AssemblyInformationalVersion("1.2.0")]
 
 
