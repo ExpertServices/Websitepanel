f17407c1132536458e5f6b0dbc4aa8bf980ac955
ptsurbeleu <ptsurbeleu@localhost>
2012-02-15 00:51:59 -0800

    Updated setup components with v1.2.1 requirement for the installer

--- WebsitePanel.Installer/Sources/WebsitePanel.Setup/EnterpriseServer10.cs
+++ WebsitePanel.Installer/Sources/WebsitePanel.Setup/EnterpriseServer10.cs
@@ -6,58 +6,85 @@ using WebsitePanel.Setup.Actions;
 
 namespace WebsitePanel.Setup
 {
-	/// <summary>
-	/// Release 1.2.0
-	/// </summary>
-	public class EnterpriseServer120 : EnterpriseServer
-	{
-		public static new object Install(object obj)
-		{
-			//
-			return EnterpriseServer.InstallBase(obj, "1.1.0");
-		}
+    /// <summary>
+    /// Release 1.2.1
+    /// </summary>
+    public class EnterpriseServer121 : EnterpriseServer
+    {
+        public static new object Install(object obj)
+        {
+            //
+            return EnterpriseServer.InstallBase(obj, "1.2.1");
+        }
 
-		public static new DialogResult Uninstall(object obj)
-		{
-			return EnterpriseServer.Uninstall(obj);
-		}
+        public static new DialogResult Uninstall(object obj)
+        {
+            return EnterpriseServer.Uninstall(obj);
+        }
 
-		public static new DialogResult Setup(object obj)
-		{
-			return EnterpriseServer.Setup(obj);
-		}
+        public static new DialogResult Setup(object obj)
+        {
+            return EnterpriseServer.Setup(obj);
+        }
 
-		public static new DialogResult Update(object obj)
-		{
+        public static new DialogResult Update(object obj)
+        {
+            return UpdateBase(obj, "1.2.1", "1.2.0", true);
+        }
+    }
+
+    /// <summary>
+    /// Release 1.2.0
+    /// </summary>
+    public class EnterpriseServer120 : EnterpriseServer
+    {
+        public static new object Install(object obj)
+        {
+            //
+            return EnterpriseServer.InstallBase(obj, "1.1.0");
+        }
+
+        public static new DialogResult Uninstall(object obj)
+        {
+            return EnterpriseServer.Uninstall(obj);
+        }
+
+        public static new DialogResult Setup(object obj)
+        {
+            return EnterpriseServer.Setup(obj);
+        }
+
+        public static new DialogResult Update(object obj)
+        {
             return UpdateBase(obj, "1.2.0", "1.1.2,1.2.0", true, new InstallAction(ActionTypes.SwitchEntServer2AspNet40));
-		}
-	}
+        }
+    }
 
-	/// <summary>
-	/// Release 1.1.0
-	/// </summary>
-	public class EnterpriseServer110 : EnterpriseServer
-	{
-		public static new object Install(object obj)
-		{
-			return EnterpriseServer.InstallBase(obj, "1.1.0");
-		}
+    /// <summary>
+    /// Release 1.1.0
+    /// </summary>
+    public class EnterpriseServer110 : EnterpriseServer
+    {
+        public static new object Install(object obj)
+        {
+            return EnterpriseServer.InstallBase(obj, "1.1.0");
+        }
 
-		public static new DialogResult Uninstall(object obj)
-		{
-			return EnterpriseServer.Uninstall(obj);
-		}
+        public static new DialogResult Uninstall(object obj)
+        {
+            return EnterpriseServer.Uninstall(obj);
+        }
 
-		public static new DialogResult Setup(object obj)
-		{
-			return EnterpriseServer.Setup(obj);
-		}
+        public static new DialogResult Setup(object obj)
+        {
+            return EnterpriseServer.Setup(obj);
+        }
 
-		public static new DialogResult Update(object obj)
-		{
-			return UpdateBase(obj, "1.1.0", "1.0.2", true);
-		}
-	}
+        public static new DialogResult Update(object obj)
+        {
+            return UpdateBase(obj, "1.1.0", "1.0.2", true);
+        }
+    }
 
     /// Release 1.0.2
     /// </summary>
