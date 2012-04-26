a364a3be468e4b0ec19e5eec30fca0c659dc90a1
ptsurbeleu <ptsurbeleu@localhost>
2012-04-26 02:21:18 -0700

    Switched to use 64-bit version of MsBuild

--- WebsitePanel/deploy-debug.bat
+++ WebsitePanel/deploy-debug.bat
@@ -1,1 +1,1 @@
-%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe build.xml /target:Deploy /property:BuildConfiguration=Debug /v:n /fileLogger
+%windir%\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe build.xml /target:Deploy /property:BuildConfiguration=Debug /v:n /fileLogger
