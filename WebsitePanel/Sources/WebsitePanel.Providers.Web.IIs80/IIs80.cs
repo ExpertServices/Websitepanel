453fce39a00d45bfdf76108008b030de41d45335
feodor_fitsner <feodor_fitsner@localhost>
2012-09-13 19:51:24 -0700

    Added separate providers for IIS 8 and FTP 8

---
+++ WebsitePanel/Sources/WebsitePanel.Providers.Web.IIs80/IIs80.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebsitePanel.Providers.Web
+{
+    public class IIs80 : IIs70, IWebServer
+    {
+        public new bool IsIISInstalled()
+        {
+            int value = 0;
+            RegistryKey root = Registry.LocalMachine;
+            RegistryKey rk = root.OpenSubKey("SOFTWARE\\Microsoft\\InetStp");
+            if (rk != null)
+            {
+                value = (int)rk.GetValue("MajorVersion", null);
+                rk.Close();
+            }
+
+            return value == 8;
+        }
+
+        public override bool IsInstalled()
+        {
+            return IsIISInstalled();
+        }
+    }
+}
