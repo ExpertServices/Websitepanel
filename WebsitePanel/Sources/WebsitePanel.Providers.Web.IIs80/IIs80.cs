b8ecd1e9eb3bcd8684b25cd14c7423c5f4f020ef
feodor_fitsner <feodor_fitsner@localhost>
2012-09-13 21:04:27 -0700

    Fixed standalone installer to correctly detect IIS 8

---
+++ WebsitePanel/Sources/WebsitePanel.Providers.Web.IIs80/IIs80.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebsitePanel.Providers.Web
+{
+    public class IIs80 : IIs70, IWebServer
+    {
+        public IIs80() : base()
+        {
+        }
+
+        public override bool IsIISInstalled()
+        {
+            int value = 0;
+            RegistryKey root = Registry.LocalMachine;
+            RegistryKey rk = root.OpenSubKey("SOFTWARE\\Microsoft\\InetStp");
+            if (rk != null)
+            {
+                value = (int)rk.GetValue("MajorVersion", null);
+                rk.Close();
+            }
+
+            return value == 8;
+        }
+
+        public override bool IsInstalled()
+        {
+            return IsIISInstalled();
+        }
+    }
+}
