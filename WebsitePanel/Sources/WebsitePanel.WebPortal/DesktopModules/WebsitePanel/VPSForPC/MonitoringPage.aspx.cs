9aca2568c673714c74a40817d13f5c652131a7bb
feodor_fitsner <feodor_fitsner@localhost>
2012-09-13 18:20:44 -0700

    Session validation key support added into installer

---
+++ WebsitePanel/Sources/WebsitePanel.WebPortal/DesktopModules/WebsitePanel/VPSForPC/MonitoringPage.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.DataVisualization.Charting;
+using WebsitePanel.Providers.Virtualization;
+
+namespace WebsitePanel.Portal.VPSForPC
+{
+    public partial class MonitoringPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+			if (Page.IsPostBack == false)
+			{
+				SetDefaultMonitoringTimeFrame();
+
+				BindPerformanceValues();
+			}
+        }
+
+		private void SetDefaultMonitoringTimeFrame()
+		{
+			if (String.IsNullOrEmpty(txtStartPeriod.Text))
+			{
+				txtStartPeriod.Text = DateTime.Now.AddDays(-1).ToShortDateString();
+			}
+			//
+			if (String.IsNullOrEmpty(txtEndPeriod.Text))
+			{
+				txtEndPeriod.Text = DateTime.Now.ToShortDateString();
+			}
+		}
+
+		private void BindPerformanceValues()
+		{
+			DateTime StartP = Convert.ToDateTime(txtStartPeriod.Text);
+			DateTime EndP = Convert.ToDateTime(txtEndPeriod.Text);
+
+			EndP = (EndP.CompareTo(DateTime.Now.Date) == 0 ? DateTime.Now : EndP);
+
+			PerformanceType pt = PerformanceType.Processor;
+
+			string charType = Page.Request.QueryString["chartType"];
+
+			InitControls(charType, StartP, EndP);
+
+			switch (charType)
+			{
+				case "Processor":
+					pt = PerformanceType.Processor;
+					break;
+				case "Network":
+					pt = PerformanceType.Network;
+					break;
+				case "Memory":
+					pt = PerformanceType.Memory;
+					break;
+			}
+
+			LoadChartData(ChartCounter, pt, StartP, EndP);
+		}
+
+		protected void operationTimer_Tick(object sender, EventArgs e)
+		{
+			BindPerformanceValues();
+		}
+
+        private void LoadChartData(Chart control, PerformanceType perfType, DateTime startPeriod, DateTime endPeriod)
+        {
+            PerformanceDataValue[] perfValues = ES.Services.VPSPC.GetPerfomanceValue(PanelRequest.ItemID, perfType, startPeriod, endPeriod);
+
+            if (perfValues != null)
+            {
+                foreach (PerformanceDataValue item in perfValues)
+                {
+					//
+					control.Series["series"].Points.AddXY(item.TimeSampled.ToString(), item.SampleValue);
+                }
+            }
+        }
+
+        private void InitControls(string charType, DateTime startPeriod, DateTime endPeriod)
+        {
+            ChartCounter.Titles.Add(charType);
+            ChartCounter.Series["series"].ChartType = (charType.Equals("Processor") ? SeriesChartType.Line : SeriesChartType.SplineArea);
+            //ChartCounter.Series["series"].IsValueShownAsLabel = true;
+            ChartCounter.Series["series"].Color = (!charType.Equals("Memory") ? System.Drawing.Color.FromArgb(220, 65, 140, 240) : ChartCounter.Series["series"].Color);
+            ChartCounter.Series["series"]["ShowMarkerLines"] = "True";
+
+            ChartCounter.ChartAreas["chartArea"].AxisX.IsMarginVisible = true;
+        }
+    }
+}
