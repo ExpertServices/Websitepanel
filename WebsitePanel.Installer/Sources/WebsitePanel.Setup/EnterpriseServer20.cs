ac75b70dc28500cb80db0de24e0daba32c9c0592
Virtuworks <Virtuworks@localhost>
2012-09-18 17:48:14 -0400

    Merge

---
+++ WebsitePanel.Installer/Sources/WebsitePanel.Setup/EnterpriseServer20.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using WebsitePanel.Setup.Actions;
+
+namespace WebsitePanel.Setup
+{
+    /// <summary>
+    /// Release 2.0.0
+    /// </summary>
+    public class EnterpriseServer200 : EnterpriseServer
+    {
+        public static new object Install(object obj)
+        {
+            //
+            return EnterpriseServer.InstallBase(obj, minimalInstallerVersion: "2.0.0");
+        }
+
+        public static new DialogResult Uninstall(object obj)
+        {
+            return EnterpriseServer.Uninstall(obj);
+        }
+
+        public static new DialogResult Setup(object obj)
+        {
+            return EnterpriseServer.Setup(obj);
+        }
+
+        public static new DialogResult Update(object obj)
+        {
+            return UpdateBase(obj,
+                minimalInstallerVersion: "2.0.0",
+                versionToUpgrade: "1.2.1",
+                updateSql: true);
+        }
+    }
+}
---
+++ WebsitePanel/Sources/WebsitePanel.Providers.FTP.IIs80/MsFTP80.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebsitePanel.Providers.FTP
+{
+    public class MsFTP80 : MsFTP
+    {
+        public MsFTP80() : base()
+        {
+        }
+
+        protected override bool IsMsFTPInstalled()
+        {
+            int value = 0;
+            RegistryKey root = Registry.LocalMachine;
+            RegistryKey rk = root.OpenSubKey("SOFTWARE\\Microsoft\\InetStp");
+            if (rk != null)
+            {
+                value = (int)rk.GetValue("MajorVersion", null);
+                rk.Close();
+            }
+
+            RegistryKey ftp = root.OpenSubKey("SYSTEM\\CurrentControlSet\\Services\\ftpsvc");
+            bool res = (value == 8) && ftp != null;
+            if (ftp != null)
+                ftp.Close();
+
+            return res;
+        }
+
+        public override bool IsInstalled()
+        {
+            return IsMsFTPInstalled();
+        }
+    }
+}
