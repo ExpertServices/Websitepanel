ea11edac93e88a8f3311cc7dce1e9ce896c20545
Virtuworks <Virtuworks@localhost>
2012-09-19 23:45:18 -0400

    Merge

---
+++ WebsitePanel.Installer/Sources/WebsitePanel.Setup/StandaloneServerSetup20.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WebsitePanel.Setup
+{
+    /// <summary>
+    /// Release 2.0.0
+    /// </summary>
+    public class StandaloneServerSetup200 : StandaloneServerSetup
+    {
+        public static new object Install(object obj)
+        {
+            return StandaloneServerSetup.InstallBase(obj, minimalInstallerVersion: "2.0.0");
+        }
+    }
+}
