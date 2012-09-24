30a2142a8bd36d6823115ad7c4202e326d5b31ae
ruslanht <ruslan@helicontech.com>
2012-09-24 14:22:06 +0300

    Web App Gallery: Setting of DB policies for multiply databases fixed

---
+++ WebsitePanel.Installer/Sources/WebsitePanel.Setup/StandaloneServerSetup10.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WebsitePanel.Setup
+{
+    /// <summary>
+    /// Release 1.2.2
+    /// </summary>
+    public class StandaloneServerSetup122 : StandaloneServerSetup
+    {
+        public static new object Install(object obj)
+        {
+            return StandaloneServerSetup.InstallBase(obj, "1.2.2");
+        }
+    }
+
+
+    /// <summary>
+    /// Release 1.2.1
+    /// </summary>
+    public class StandaloneServerSetup121 : StandaloneServerSetup
+    {
+        public static new object Install(object obj)
+        {
+            return StandaloneServerSetup.InstallBase(obj, "1.2.1");
+        }
+    }
+
+    /// <summary>
+    /// Release 1.2.0
+    /// </summary>
+    public class StandaloneServerSetup120 : StandaloneServerSetup
+    {
+        public static new object Install(object obj)
+        {
+            return StandaloneServerSetup.InstallBase(obj, "1.2.0");
+        }
+    }
+
+    /// <summary>
+    /// Release 1.1.0
+    /// </summary>
+    public class StandaloneServerSetup110 : StandaloneServerSetup
+    {
+        public static new object Install(object obj)
+        {
+            return StandaloneServerSetup.InstallBase(obj, "1.1.0");
+        }
+    }
+
+    /// <summary>
+    /// Release 1.0.2
+    /// </summary>
+    public class StandaloneServerSetup102 : StandaloneServerSetup101
+    {
+        public static new object Install(object obj)
+        {
+            return StandaloneServerSetup.InstallBase(obj, "1.0.0");
+        }
+    }
+
+    /// <summary>
+    /// Release 1.0.1
+    /// </summary>
+    public class StandaloneServerSetup101 : StandaloneServerSetup
+    {
+        public static new object Install(object obj)
+        {
+            return StandaloneServerSetup.InstallBase(obj, "1.0.0");
+        }
+    }
+
+    /// <summary>
+    /// Release 1.0
+    /// </summary>
+    public class StandaloneServerSetup10 : StandaloneServerSetup
+    {
+        public static new object Install(object obj)
+        {
+            return StandaloneServerSetup.InstallBase(obj, "1.0.0");
+        }
+    }
+}
