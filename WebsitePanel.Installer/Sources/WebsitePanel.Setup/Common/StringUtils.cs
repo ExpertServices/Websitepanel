ca32759d8235ff155576e42d40d2a3e98dc1ad6b
feodor_fitsner <feodor_fitsner@localhost>
2012-10-05 11:08:18 -0700

    GenerateNextAvailableIP fixed

---
+++ WebsitePanel.Installer/Sources/WebsitePanel.Setup/Common/StringUtils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace WebsitePanel.Setup.Common
+{
+    public class StringUtils
+    {
+        public static string GenerateRandomString(int length)
+        {
+            RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider();
+            byte[] data = new byte[length];
+            crypto.GetNonZeroBytes(data);
+            return BitConverter.ToString(data).Replace("-", "").ToLowerInvariant();
+        }
+    }
+}
