12e342aa66ed6362216760cd8a329c687dbea5fd
robvde <robvde@ROBVDE913.europe.corp.microsoft.com>
2012-11-11 22:44:43 +0400

    Update: Lync: Support for multiple sipdomains per tenant. Fixed: Hosted Organizations: user principal name updated when settings primary email address. User principal name will not change when changing the primary email address.
    SIP address is based on EmailAddress

---
+++ WebsitePanel.Installer/Sources/WebsitePanel.Setup/Server20.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebsitePanel.Setup
+{
+    /// <summary>
+    /// Release 2.0.0
+    /// </summary>
+    public class Server200 : Server
+    {
+        public static new object Install(object obj)
+        {
+            //
+            return Server.InstallBase(obj, minimalInstallerVersion: "2.0.0");
+        }
+
+        public static new object Uninstall(object obj)
+        {
+            return Server.Uninstall(obj);
+        }
+
+        public static new object Setup(object obj)
+        {
+            return Server.Setup(obj);
+        }
+
+        public static new object Update(object obj)
+        {
+            return Server.UpdateBase(obj,
+                minimalInstallerVersion: "2.0.0",
+                versionToUpgrade: "1.2.1,2.0.0",
+                updateSql: false);
+        }
+    }
+}
