36afd274ed77a6ceb6c50842097e9b739fc2d299
ruslanht <ruslan@helicontech.com>
2012-12-18 16:41:21 +0200

    Helicon Ape: Server-wide enbaling/disabling (global registration) added

--- WebsitePanel/Sources/WebsitePanel.WebPortal/DesktopModules/WebsitePanel/ProviderControls/IIS70_Settings.ascx.designer.cs
+++ WebsitePanel/Sources/WebsitePanel.WebPortal/DesktopModules/WebsitePanel/ProviderControls/IIS70_Settings.ascx.designer.cs
@@ -742,6 +742,15 @@ namespace WebsitePanel.Portal.ProviderControls {
         protected global::System.Web.UI.WebControls.Button EditHeliconApeConfButton;
         
         /// <summary>
+        /// chkHeliconApeGlobalRegistration control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox chkHeliconApeGlobalRegistration;
+        
+        /// <summary>
         /// secOther control.
         /// </summary>
         /// <remarks>
