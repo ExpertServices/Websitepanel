4714f6c3cee87ba0867ca499b80c24c26104910e
Virtuworks <Virtuworks@localhost>
2013-01-10 16:30:08 -0500

    Merge

--- WebsitePanel.WHMCSModule/includes/hooks/websitepanel_addons.php
+++ WebsitePanel.WHMCSModule/includes/hooks/websitepanel_addons.php
@@ -54,7 +54,7 @@ function websitepanel_addons_AddonActivation($params)
         
         // Retrieve the WebsitePanel Addons module settings
         $modSettings = websitepanel_addons_GetSettings();
-        if (empty($modSettings['username']) || empty($modSettings['password'])) || empty($modSettings['serverhost'])) || empty($modSettings['serverport'])))
+        if (empty($modSettings['username']) || empty($modSettings['password']) || empty($modSettings['serverhost']) || empty($modSettings['serverport']))
         {
             // The module is disabled or has not yet been configured - stop
             return;
