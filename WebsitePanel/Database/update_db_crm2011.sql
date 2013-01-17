3047dea4ec164b90fd936df1e2968dee3c0232ad
dev <dev@amd-telecom.net>
2013-01-17 08:54:31 +0400

    add crm 2011 support

---
+++ WebsitePanel/Database/update_db_crm2011.sql
@@ -0,0 +1,2 @@
+﻿INSERT [dbo].[Providers] ([ProviderID], [GroupID], [ProviderName], [DisplayName], [ProviderType], [EditorControl], [DisableAutoDiscovery]) VALUES (1201, 21, N'CRM', N'Hosted MS CRM 2011', N'WebsitePanel.Providers.HostedSolution.CRMProvider2011, WebsitePanel.Providers.HostedSolution', N'CRM', NULL)
+GO
