9fa95cce2d871523785e49623c919df4e50a022b
Virtuworks <Virtuworks@localhost>
2013-01-23 16:12:13 -0500

    Fix for "Object reference error" with WebSitesHeliconApe

---
+++ WebsitePanel.HyperV.Utils/Sources/WebsitePanel.HyperV.Utils/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Hyper-V Utilities")]
+[assembly: AssemblyDescription("Utility to work with Hyper-V virtual resources.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("VmUtils")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c72a836b-0d6e-482f-afba-8c03964d9933")]
---
+++ WebsitePanel.VmConfig/Sources/WebsitePanel.VmConfig/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WebsitePanel Virtual Machine Configuration Service")]
+[assembly: AssemblyDescription("Performs provisioning operations in the guest operating system.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("WebsitePanel VPS Solution")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("52e42d75-bcac-406f-b3ed-31d19265ea22")]
