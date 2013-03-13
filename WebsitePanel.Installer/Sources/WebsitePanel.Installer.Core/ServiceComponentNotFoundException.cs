3639a350e535dd19e2907849bdb878cd27d8079a
ptsurbeleu <ptsurbeleu@localhost>
2013-03-12 23:19:10 -0700

    BUILD: Added 7-Zip to the build tools location

---
+++ WebsitePanel.Installer/Sources/WebsitePanel.Installer.Core/ServiceComponentNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebsitePanel.Installer.Core
+{
+	class ServiceComponentNotFoundException : Exception
+	{
+		public ServiceComponentNotFoundException(string p)
+			: base(p)
+		{
+		}
+	}
+}
