34e9f32267e91f9427676ec6fbb508ec6c7fcc66
Virtuworks <Virtuworks@localhost>
2013-04-16 10:40:58 -0400

    Fix issue with Web.config

---
+++ WebsitePanel/deploy-release.bat
@@ -0,0 +1,1 @@
+%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe build.xml /target:Deploy /p:BuildConfiguration=Release /p:Version="2.1.0" /p:FileVersion="2.1.0.1" /p:VersionLabel="2.1.0.1" /v:n /fileLogger /m
