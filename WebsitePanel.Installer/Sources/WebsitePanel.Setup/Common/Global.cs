a36f567c6d3d3e04a8293580b62f5cfb07b41fde
vfedosevich <vfedosevich@localhost>
2013-05-03 17:29:25 +0300

    Scheduler moved into windows service.

--- WebsitePanel.Installer/Sources/WebsitePanel.Setup/Common/Global.cs
+++ WebsitePanel.Installer/Sources/WebsitePanel.Setup/Common/Global.cs
@@ -77,6 +77,8 @@ namespace WebsitePanel.Setup
 			public const string ConnectionString = "ConnectionString";
 			public const string InstallConnectionString = "InstallConnectionString";
 			public const string Release = "Release";
+		    public const string SchedulerServiceFileName = "WebsitePanel.SchedulerService.exe";
+            public const string SchedulerServiceName = "WebsitePanel Scheduler";
 		}
 
 		public abstract class Messages
--- WebsitePanel/Sources/WebsitePanel.SchedulerService/SchedulerService.cs
+++ WebsitePanel/Sources/WebsitePanel.SchedulerService/SchedulerService.cs
@@ -1,23 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Linq;
-using System.ServiceProcess;
-using System.Text;
-using System.Timers;
+﻿using System.ServiceProcess;
+using WebsitePanel.EnterpriseServer;
 
 namespace WebsitePanel.SchedulerService
 {
     public partial class SchedulerService : ServiceBase
     {
-        #region Properties
-
-        public Timer _Timer { get; protected set; }
-
-        #endregion
-
         #region Construcor
 
         public SchedulerService()
@@ -31,15 +18,7 @@ namespace WebsitePanel.SchedulerService
 
         protected override void OnStart(string[] args)
         {
-        }
-
-        protected override void OnStop()
-        {
-            _Timer.Dispose();
-        }
-
-        protected void Porcess(object state)
-        {
+            Scheduler.Start();
         }
 
         #endregion
