a87805acf71858e54a34bed310b73c070682daf6
dev_amdtel <dev_amdtel@localhost>
2013-05-14 23:28:03 +0400

    add Application Pools Restart Quota

---
+++ WebsitePanel/Sources/WebsitePanel.Providers.Base/Common/AppPoolState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WebsitePanel.Providers
+{
+    [Serializable]
+    public enum AppPoolState
+    {
+        Unknown = 0,
+        Starting = 1,
+        Started = 2,
+        Stopping = 3,
+        Stopped = 4,
+        Recycle = 5
+    }
+}
