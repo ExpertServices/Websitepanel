b6352f2cc8f829a3dfc438b964bf5e057c95f3b4
Virtuworks <Virtuworks@localhost>
2013-05-20 11:23:47 -0400

    Merge Commit

--- WebsitePanel.WHMCSModule/includes/hooks/websitepanel_sync.php
+++ WebsitePanel.WHMCSModule/includes/hooks/websitepanel_sync.php
@@ -65,7 +65,7 @@ function websitepanel_sync_ClientEdit($params)
         $wsp = new WebsitePanel($srvSettings['username'], $srvSettings['password'], $modSettings['serverhost'], $modSettings['serverport'], (($modSettings['serversecured']) == 'on' ? TRUE : FALSE));
         
         // Get all WSP users with the old email
-        $items = (array)$wsp->get_users_paged_recursive(1, 'Email', $params['olddata']['email'], 0, 0, '');
+        $items = (array)$wsp->getUsersPagedRecursive(1, 'Email', $params['olddata']['email'], 0, 0, '');
                 
         // Load / parse the XML response
         $xml = simplexml_load_string($items['any']);
@@ -80,10 +80,10 @@ function websitepanel_sync_ClientEdit($params)
             // Set the current root element and get the users details from WebsitePanel
             // We cannot use the details provided by the get_users_paged_recursive method as it does not return all the required elements to fully update the user
             $currentRoot = $rootPath->Table1->$i;
-            $userDetails = (array)$wsp->get_user_by_username($currentRoot->Username);
+            $userDetails = (array)$wsp->getUserByUsername($currentRoot->Username);
             
             // Update the current user
-            $wsp->update_user_details($userDetails['RoleId'], (($userDetails['RoleId'] == 2) ? 'Reseller' : 'User'), $userDetails['StatusId'], $userDetails['Status'], $userDetails['LoginStatusId'], $userDetails['LoginStatus'], $userDetails['FailedLogins'], $userDetails['UserId'], $userDetails['OwnerId'], $userDetails['Created'], $userDetails['Changed'], $userDetails['IsDemo'], $userDetails['IsPeer'], $currentRoot->Comments, $userDetails['Username'], $userDetails['Password'], $params['firstname'], $params['lastname'], $params['email'], $params['phonenumber'], $params['postcode'], '', '', '', '', $params['country'], $params['address1'] . (!empty($params['address2']) ? " {$params['address2']}" : ''), $params['city'], $params['state'], TRUE, $params['companyname'], (($userDetails['RoleId'] == 2) ? TRUE : FALSE));
+            $wsp->updateUserDetails($userDetails['RoleId'], (($userDetails['RoleId'] == 2) ? 'Reseller' : 'User'), $userDetails['StatusId'], $userDetails['Status'], $userDetails['LoginStatusId'], $userDetails['LoginStatus'], $userDetails['FailedLogins'], $userDetails['UserId'], $userDetails['OwnerId'], $userDetails['Created'], $userDetails['Changed'], $userDetails['IsDemo'], $userDetails['IsPeer'], $currentRoot->Comments, $userDetails['Username'], $userDetails['Password'], $params['firstname'], $params['lastname'], $params['email'], $params['phonenumber'], $params['postcode'], '', '', '', '', $params['country'], $params['address1'] . (!empty($params['address2']) ? " {$params['address2']}" : ''), $params['city'], $params['state'], TRUE, $params['companyname'], (($userDetails['RoleId'] == 2) ? TRUE : FALSE));
             
             // Add log entry to client log
             logactivity("WebsitePanel Sync - Account {$currentRoot->Username} contact details updated successfully", $params['userid']);
