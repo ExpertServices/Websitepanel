a75ca911aa9b89b69ee1ceac96524270603b2925
vfedosevich <vfedosevich@localhost>
2013-06-18 12:08:11 +0300

    Release bug fixing

---
+++ WebsitePanel/Sources/WebsitePanel.SchedulerService/SchedulerService.cs
@@ -0,0 +1,42 @@
+﻿using System.ServiceProcess;
+using System.Threading;
+using WebsitePanel.EnterpriseServer;
+
+namespace WebsitePanel.SchedulerService
+{
+    public partial class SchedulerService : ServiceBase
+    {
+        private Timer _Timer;
+        private static bool _isRuninng;
+        #region Construcor
+
+        public SchedulerService()
+        {
+            InitializeComponent();
+
+            _Timer = new Timer(Process, null, 5000, 5000);
+            _isRuninng = false;
+        }
+
+        #endregion
+
+        #region Methods
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected static void Process(object callback)
+        {
+            //check running service
+            if (_isRuninng)
+                return;
+
+            _isRuninng = true;
+             Scheduler.Start();
+            _isRuninng = false;
+        }
+
+        #endregion
+    }
+}
