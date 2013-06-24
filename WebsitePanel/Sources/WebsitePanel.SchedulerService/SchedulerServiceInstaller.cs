d97a1c79da4fb81b3b14b552a582d40a95cde9a9
vfedosevich <vfedosevich@localhost>
2013-06-24 09:49:37 +0300

    MSI output file name changed

---
+++ WebsitePanel/Sources/WebsitePanel.SchedulerService/SchedulerServiceInstaller.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
+
+namespace WebsitePanel.SchedulerService
+{
+    [RunInstaller(true)]
+    public class SchedulerServiceInstaller : Installer
+    {
+        #region Constructor
+
+        public SchedulerServiceInstaller()
+        {
+            var processInstaller = new ServiceProcessInstaller();
+            var serviceInstaller = new ServiceInstaller();
+
+            processInstaller.Account = ServiceAccount.LocalSystem;
+            serviceInstaller.DisplayName = "WebsitePanel Scheduler";
+            serviceInstaller.StartType = ServiceStartMode.Automatic;
+            serviceInstaller.ServiceName = "WebsitePanel Scheduler";
+
+            Installers.Add(processInstaller);
+            Installers.Add(serviceInstaller);
+        }
+
+        #endregion
+    }
+}
