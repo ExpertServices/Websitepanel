a9a2a5a54e85616c1f1452289a4090574dacf0bd
Virtuworks <Virtuworks@localhost>
2013-06-27 17:08:52 -0400

    Added tag build-2.1.0.126 for changeset d8433acd5b3d

---
+++ WebsitePanel/Sources/WebsitePanel.Providers.Base/HostedSolution/ExchangeDisclaimer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebsitePanel.Providers.HostedSolution
+{
+    public class ExchangeDisclaimer
+    {
+        int exchangeDisclaimerId;
+        int itemId;
+        string disclaimerName;
+        string disclaimerText;
+
+        public int ItemId
+        {
+            get { return this.itemId; }
+            set { this.itemId = value; }
+        }
+
+        public int ExchangeDisclaimerId
+        {
+            get { return this.exchangeDisclaimerId; }
+            set { this.exchangeDisclaimerId = value; }
+        }
+
+        public string DisclaimerName
+        {
+            get { return this.disclaimerName; }
+            set { this.disclaimerName = value; }
+        }
+
+        public string DisclaimerText
+        {
+            get { return this.disclaimerText; }
+            set { this.disclaimerText = value; }
+        }
+
+    }
+}
---
+++ WebsitePanel/Sources/WebsitePanel.WebPortal/DesktopModules/WebsitePanel/ProviderControls/CRM2011_Settings.ascx.designer.cs
@@ -0,0 +1,96 @@
+//------------------------------------------------------------------------------
+// <автоматически создаваемое>
+//     Этот код создан программой.
+//
+//     Изменения в этом файле могут привести к неправильной работе и будут потеряны в случае
+//     повторной генерации кода. 
+// </автоматически создаваемое>
+//------------------------------------------------------------------------------
+
+namespace WebsitePanel.Portal.ProviderControls {
+    
+    
+    public partial class CRM2011_Settings {
+        
+        /// <summary>
+        /// txtSqlServer элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtSqlServer;
+        
+        /// <summary>
+        /// RequiredFieldValidator1 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+        
+        /// <summary>
+        /// txtReportingService элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtReportingService;
+        
+        /// <summary>
+        /// txtDomainName элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDomainName;
+        
+        /// <summary>
+        /// RequiredFieldValidator2 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator2;
+        
+        /// <summary>
+        /// ddlSchema элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlSchema;
+        
+        /// <summary>
+        /// ddlCrmIpAddress элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::WebsitePanel.Portal.SelectIPAddress ddlCrmIpAddress;
+        
+        /// <summary>
+        /// txtPort элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPort;
+        
+        /// <summary>
+        /// txtAppRootDomain элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtAppRootDomain;
+    }
+}
