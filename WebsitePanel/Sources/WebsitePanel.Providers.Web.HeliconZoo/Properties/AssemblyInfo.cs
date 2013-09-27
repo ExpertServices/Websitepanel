a8254420b911942ca283cbf35412dedf5917ed0d
vfedosevich <vfedosevich@localhost>
2013-09-27 16:53:59 +0300

    fixed issue with quotas

---
+++ WebsitePanel/Sources/WebsitePanel.Providers.Web.HeliconZoo/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WebsitePanel.Providers.Web.HeliconZoo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("WebsitePanel.Providers.Web.HeliconZoo")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("36ce3717-bdb5-40c3-ba56-c91ab9bf1903")]
+
