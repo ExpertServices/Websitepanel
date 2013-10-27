71770c2341cd6fc0bdfa75256988bdc1dcf20efc
Virtuworks <Virtuworks@localhost>
2013-10-27 08:21:25 -0400

    Added tag build-2.1.0.199 for changeset 0625b6908fb5

---
+++ WebsitePanel/Sources/WebsitePanel.EnterpriseServer/esEnterpriseStorage.asmx.cs
@@ -0,0 +1,95 @@
+// Copyright (c) 2012, Outercurve Foundation.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+// - Redistributions of source code must  retain  the  above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// - Redistributions in binary form  must  reproduce the  above  copyright  notice,
+//   this list of conditions  and  the  following  disclaimer in  the documentation
+//   and/or other materials provided with the distribution.
+//
+// - Neither  the  name  of  the  Outercurve Foundation  nor   the   names  of  its
+//   contributors may be used to endorse or  promote  products  derived  from  this
+//   software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,  BUT  NOT  LIMITED TO, THE IMPLIED
+// WARRANTIES  OF  MERCHANTABILITY   AND  FITNESS  FOR  A  PARTICULAR  PURPOSE  ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL,  SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO,  PROCUREMENT  OF  SUBSTITUTE  GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  HOWEVER  CAUSED AND ON
+// ANY  THEORY  OF  LIABILITY,  WHETHER  IN  CONTRACT,  STRICT  LIABILITY,  OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE)  ARISING  IN  ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+using System;
+using System.Data;
+using System.Web;
+using System.Collections;
+using System.Collections.Generic;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+using Microsoft.Web.Services3;
+
+using WebsitePanel.Providers.Common;
+using WebsitePanel.Providers.HostedSolution;
+using WebsitePanel.Providers.EnterpriseStorage;
+using WebsitePanel.Providers.ResultObjects;
+using WebsitePanel.Providers.OS;
+
+namespace WebsitePanel.EnterpriseServer
+{
+    /// <summary>
+    /// Summary description for esEnterpriseStorage
+    /// </summary>
+    [WebService(Namespace = "http://smbsaas/websitepanel/enterpriseserver")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [Policy("ServerPolicy")]
+    [ToolboxItem(false)]
+    public class esEnterpriseStorage : WebService
+    {
+
+        [WebMethod]
+        public bool CheckFileServicesInstallation(int serviceId)
+        {
+            return EnterpriseStorageController.CheckFileServicesInstallation(serviceId);
+        }
+
+        [WebMethod]
+        public SystemFile[] GetEnterpriseFolders(int itemId)
+        {
+            return EnterpriseStorageController.GetFolders(itemId);
+        }
+
+        [WebMethod]
+        public SystemFile GetEnterpriseFolder(int itemId, string folderName)
+        {
+            return EnterpriseStorageController.GetFolder(itemId, folderName);
+        }
+
+        [WebMethod]
+        public ResultObject CreateEnterpriseFolder(int itemId, string folderName, long quota)
+        {
+            return EnterpriseStorageController.CreateFolder(itemId, folderName, quota);
+        }
+       
+
+        [WebMethod]
+        public ResultObject DeleteEnterpriseFolder(int itemId, string folderName)
+        {
+            return EnterpriseStorageController.DeleteFolder(itemId, folderName);
+        }
+
+        [WebMethod]
+        public ResultObject SetEnterpriseFolderQuota(int itemId, string folderName, long quota)
+        {
+            return EnterpriseStorageController.SetFolderQuota(itemId, folderName, quota);
+        }
+    }
+}
