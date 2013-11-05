53f3e80e0fdfbea15d135e63432e26f86903f436
Virtuworks <Virtuworks@localhost>
2013-11-05 18:48:05 -0500

    Added tag build-2.1.0.210 for changeset bbece8cb8cea

---
+++ WebsitePanel/Sources/WebsitePanel.WebPortal/DesktopModules/WebsitePanel/ExchangeServer/EnterpriseStorageFolderGeneralSettings.ascx
@@ -0,0 +1,71 @@
+<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="EnterpriseStorageFolderGeneralSettings.ascx.cs" Inherits="WebsitePanel.Portal.ExchangeServer.EnterpriseStorageFolderGeneralSettings" %>
+<%@ Register Src="../UserControls/SimpleMessageBox.ascx" TagName="SimpleMessageBox" TagPrefix="wsp" %>
+<%@ Register Src="UserControls/EnterpriseStoragePermissions.ascx" TagName="ESPermissions" TagPrefix="wsp"%>
+<%@ Register Src="UserControls/Menu.ascx" TagName="Menu" TagPrefix="wsp" %>
+<%@ Register Src="UserControls/Breadcrumb.ascx" TagName="Breadcrumb" TagPrefix="wsp" %>
+<%@ Register TagPrefix="wsp" TagName="CollapsiblePanel" Src="../UserControls/CollapsiblePanel.ascx" %>
+<%@ Register Src="../UserControls/EnableAsyncTasksSupport.ascx" TagName="EnableAsyncTasksSupport" TagPrefix="wsp" %>
+
+<wsp:EnableAsyncTasksSupport id="asyncTasks" runat="server"/>
+
+<div id="ExchangeContainer">
+	<div class="Module">
+		<div class="Header">
+			<wsp:Breadcrumb id="breadcrumb" runat="server" PageName="Text.PageName" />
+		</div>
+		<div class="Left">
+			<wsp:Menu id="menu" runat="server" SelectedItem="esfolders" />
+		</div>
+		<div class="Content">
+			<div class="Center">
+				<div class="Title">
+					<asp:Image ID="Image1" SkinID="ExchangeList48" runat="server" />
+					<asp:Localize ID="locTitle" runat="server" meta:resourcekey="locTitle" Text="Edit Folder"></asp:Localize>
+
+					<asp:Literal ID="litFolderName" runat="server" Text="Folder" />
+                </div>
+				<div class="FormBody">
+                    <wsp:SimpleMessageBox id="messageBox" runat="server" />
+					<table>
+						<tr>
+							<td class="FormLabel150"><asp:Localize ID="locFolderName" runat="server" meta:resourcekey="locFolderName" Text="Folder Name:"></asp:Localize></td>
+							<td>
+								<asp:TextBox ID="txtFolderName" runat="server" CssClass="HugeTextBox200"></asp:TextBox>
+								<asp:RequiredFieldValidator ID="valRequireFolderName" runat="server" meta:resourcekey="valRequireFolderName" ControlToValidate="txtFolderName"
+									ErrorMessage="Enter Folder Name" ValidationGroup="EditFolder" Display="Dynamic" Text="*" SetFocusOnError="True"></asp:RequiredFieldValidator>
+                                <br />
+						        <br />
+							</td>
+						</tr>
+                        <tr>
+                            <td class="FormLabel150"><asp:Localize ID="locFolderUrl" runat="server" meta:resourcekey="locFolderUrl" Text="Folder Url:"></asp:Localize></td>
+                            <td><asp:Label runat="server" ID="lblFolderUrl" /></td>
+                        </tr>
+                        <tr><td>&nbsp;</td></tr>
+                        <tr>
+                            <td class="FormLabel150"><asp:Localize ID="locDirectoryBrowsing" runat="server" meta:resourcekey="locDirectoryBrowsing" Text="Enable Directory Browsing:"></asp:Localize></td>
+						    <td>
+							    <asp:CheckBox id="chkDirectoryBrowsing" runat="server"></asp:CheckBox>
+						    </td>
+					    </tr>
+                        <tr><td>&nbsp;</td></tr>
+						<tr>
+							<td colspan="2">
+                                <fieldset id="PermissionsPanel" runat="server">
+                                    <legend><asp:Localize ID="PermissionsSection" runat="server" meta:resourcekey="locPermissionsSection" Text="Permissions"></asp:Localize></legend>
+                                    <wsp:ESPermissions id="permissions" runat="server" />
+                                </fieldset>
+						</tr>
+					    <tr><td>&nbsp;</td></tr>
+
+					</table>
+					
+				    <div class="FormFooterClean">
+					    <asp:Button id="btnSave" runat="server" Text="Save Changes" CssClass="Button1" meta:resourcekey="btnSave" ValidationGroup="EditFolder" OnClick="btnSave_Click"></asp:Button>
+					    <asp:ValidationSummary ID="valSummary" runat="server" ShowMessageBox="True" ShowSummary="False" ValidationGroup="EditFolder" />
+				    </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ WebsitePanel/Sources/WebsitePanel.WebPortal/DesktopModules/WebsitePanel/ExchangeServer/EnterpriseStorageFolderGeneralSettings.ascx.cs
@@ -0,0 +1,129 @@
+// Copyright (c) 2012, Outercurve Foundation.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+// - Redistributions of source code must  retain  the  above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// - Redistributions in binary form  must  reproduce the  above  copyright  notice,
+//   this list of conditions  and  the  following  disclaimer in  the documentation
+//   and/or other materials provided with the distribution.
+//
+// - Neither  the  name  of  the  Outercurve Foundation  nor   the   names  of  its
+//   contributors may be used to endorse or  promote  products  derived  from  this
+//   software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,  BUT  NOT  LIMITED TO, THE IMPLIED
+// WARRANTIES  OF  MERCHANTABILITY   AND  FITNESS  FOR  A  PARTICULAR  PURPOSE  ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL,  SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO,  PROCUREMENT  OF  SUBSTITUTE  GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  HOWEVER  CAUSED AND ON
+// ANY  THEORY  OF  LIABILITY,  WHETHER  IN  CONTRACT,  STRICT  LIABILITY,  OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE)  ARISING  IN  ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
+using WebsitePanel.Providers.HostedSolution;
+using WebsitePanel.EnterpriseServer;
+using WebsitePanel.Providers.OS;
+
+namespace WebsitePanel.Portal.ExchangeServer
+{
+    public partial class EnterpriseStorageFolderGeneralSettings : WebsitePanelModuleBase
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindSettings();
+            }
+        }
+
+        private void BindSettings()
+        {
+            try
+            {
+                // get settings
+
+                Organization org = ES.Services.Organizations.GetOrganization(PanelRequest.ItemID);
+
+                SystemFile folder = ES.Services.EnterpriseStorage.GetEnterpriseFolder(
+                    PanelRequest.ItemID, PanelRequest.FolderID);
+
+                litFolderName.Text = string.Format("{0}\\{1}", org.OrganizationId, folder.Name);
+
+                // bind form
+                txtFolderName.Text = folder.Name;
+                lblFolderUrl.Text = folder.Url;
+
+                var esPermissions = ES.Services.EnterpriseStorage.GetEnterpriseFolderPermissions(PanelRequest.ItemID,folder.Name);
+
+                chkDirectoryBrowsing.Checked = ES.Services.WebServers.GetDirectoryBrowseEnabled(PanelRequest.ItemID, folder.Url);
+
+                permissions.SetPermissions(esPermissions);
+
+            }
+            catch (Exception ex)
+            {
+                messageBox.ShowErrorMessage("ENETERPRISE_STORAGE_GET_FOLDER_SETTINGS", ex);
+            }
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            if (!Page.IsValid)
+                return;
+
+            try
+            {
+                bool redirectNeeded = false;
+
+                litFolderName.Text = txtFolderName.Text;
+
+              //  SystemFile folder = ES.Services.EnterpriseStorage.GetEnterpriseFolder(PanelRequest.ItemID, PanelRequest.FolderID);
+                SystemFile folder = new SystemFile();
+
+                if (PanelRequest.FolderID != txtFolderName.Text)
+                {
+                    if (txtFolderName.Text.Contains("\\"))
+                    {
+                        throw new Exception("Wrong file name");
+                    }
+
+                    folder = ES.Services.EnterpriseStorage.RenameEnterpriseFolder(PanelRequest.ItemID, PanelRequest.FolderID, txtFolderName.Text);
+                    redirectNeeded = true;
+                }
+
+                ES.Services.EnterpriseStorage.SetEnterpriseFolderPermissions(PanelRequest.ItemID, redirectNeeded ? folder.Name : PanelRequest.FolderID, permissions.GetPemissions());
+
+                ES.Services.WebServers.SetDirectoryBrowseEnabled(PanelRequest.ItemID, redirectNeeded ? folder.Url : lblFolderUrl.Text, chkDirectoryBrowsing.Checked);
+
+                if (redirectNeeded)
+                {
+                    Response.Redirect(EditUrl("SpaceID", PanelSecurity.PackageId.ToString(), "enterprisestorage_folders",
+                        "ItemID=" + PanelRequest.ItemID));
+                }
+
+                messageBox.ShowSuccessMessage("ENTERPRISE_STORAGE_UPDATE_FOLDER_SETTINGS");
+            }
+            catch (Exception ex)
+            {
+                messageBox.ShowErrorMessage("ENTERPRISE_STORAGE_UPDATE_FOLDER_SETTINGS", ex);
+            }
+        }
+    }
+}
