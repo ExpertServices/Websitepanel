4aef970b593608eca67beb9083d3cfbbc926bb90
vfedosevich <vfedosevich@localhost>
2014-04-25 04:30:36 +0300

    ability to specify default organization

---
+++ WebsitePanel/Sources/WebsitePanel.Providers.TerminalServices.Windows2012/Windows2012.cs
@@ -0,0 +1,132 @@
+﻿// Copyright (c) 2014, Outercurve Foundation.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+// - Redistributions of source code must  retain  the  above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// - Redistributions in binary form  must  reproduce the  above  copyright  notice,
+//   this list of conditions  and  the  following  disclaimer in  the documentation
+//   and/or other materials provided with the distribution.
+//
+// - Neither  the  name  of  the  Outercurve Foundation  nor   the   names  of  its
+//   contributors may be used to endorse or  promote  products  derived  from  this
+//   software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,  BUT  NOT  LIMITED TO, THE IMPLIED
+// WARRANTIES  OF  MERCHANTABILITY   AND  FITNESS  FOR  A  PARTICULAR  PURPOSE  ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL,  SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO,  PROCUREMENT  OF  SUBSTITUTE  GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)  HOWEVER  CAUSED AND ON
+// ANY  THEORY  OF  LIABILITY,  WHETHER  IN  CONTRACT,  STRICT  LIABILITY,  OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE)  ARISING  IN  ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Win32;
+
+using WebsitePanel.Server.Utils;
+using WebsitePanel.Providers.Utils;
+using WebsitePanel.Providers.OS;
+using WebsitePanel.Providers.Common;
+
+
+namespace WebsitePanel.Providers.RemoteDesktopServices
+{
+    public class Windows2012 : HostingServiceProviderBase
+    {
+
+        #region Properties
+        protected string UsersHome
+        {
+            get { return FileUtils.EvaluateSystemVariables(ProviderSettings[Constants.UsersHome]); }
+        }
+        #endregion
+
+
+        #region HostingServiceProvider methods
+        public override string[] Install()
+        {
+            List<string> messages = new List<string>();
+
+            // create folder if it not exists
+            try
+            {
+                if (!FileUtils.DirectoryExists(UsersHome))
+                {
+                    FileUtils.CreateDirectory(UsersHome);
+                }
+            }
+            catch (Exception ex)
+            {
+                messages.Add(String.Format("Folder '{0}' could not be created: {1}",
+                    UsersHome, ex.Message));
+            }
+            return messages.ToArray();
+        }
+
+        public override void DeleteServiceItems(ServiceProviderItem[] items)
+        {
+            foreach (ServiceProviderItem item in items)
+            {
+                try
+                {
+                    if (item is HomeFolder)
+                        // delete home folder
+                        FileUtils.DeleteFile(item.Name);
+                }
+                catch (Exception ex)
+                {
+                    Log.WriteError(String.Format("Error deleting '{0}' {1}", item.Name, item.GetType().Name), ex);
+                }
+            }
+        }
+
+        public override ServiceProviderItemDiskSpace[] GetServiceItemsDiskSpace(ServiceProviderItem[] items)
+        {
+            List<ServiceProviderItemDiskSpace> itemsDiskspace = new List<ServiceProviderItemDiskSpace>();
+            foreach (ServiceProviderItem item in items)
+            {
+                if (item is HomeFolder)
+                {
+                    try
+                    {
+                        string path = item.Name;
+
+                        Log.WriteStart(String.Format("Calculating '{0}' folder size", path));
+
+                        // calculate disk space
+                        ServiceProviderItemDiskSpace diskspace = new ServiceProviderItemDiskSpace();
+                        diskspace.ItemId = item.Id;
+                        diskspace.DiskSpace = FileUtils.CalculateFolderSize(path);
+                        itemsDiskspace.Add(diskspace);
+
+                        Log.WriteEnd(String.Format("Calculating '{0}' folder size", path));
+                    }
+                    catch (Exception ex)
+                    {
+                        Log.WriteError(ex);
+                    }
+                }
+            }
+            return itemsDiskspace.ToArray();
+        }
+        #endregion
+
+        public override bool IsInstalled()
+        {
+            // TODO: Remove it.
+            return true;
+
+            Server.Utils.OS.WindowsVersion version = WebsitePanel.Server.Utils.OS.GetVersion();
+            return version == WebsitePanel.Server.Utils.OS.WindowsVersion.WindowsServer2012;
+        }
+    }
+}
