10cd759eaedbc4580230b3fe4f35df9021e382d4
Virtuworks <Virtuworks@localhost>
2014-06-08 16:24:35 -0400

    Blackberry 5 - Update Arguments

---
+++ WebsitePanel/Sources/WebsitePanel.EnterpriseServer.Base/Common/WebPlatformInstaller.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebsitePanel.EnterpriseServer.Base.Common
+{
+    public class WebPlatformInstaller
+    {
+        public const string MAIN_FEED_URL = "http://www.microsoft.com/web/webpi/4.5/WebProductList.xml";
+        public const string ZOO_FEED = "http://www.helicontech.com/zoo/feed-wsp21.xml";
+    }
+}
