f120a623e7f8b1d695afc476e2bea7a1de69d918
Virtuworks <Virtuworks@localhost>
2014-07-08 21:38:23 -0400

    Update checkbox.png

---
+++ WebsitePanel/Sources/WebsitePanel.EnterpriseServer.Base/Tasks/BackgroundTaskStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace WebsitePanel.EnterpriseServer
+{
+    public enum BackgroundTaskStatus
+    {
+        Run = 1,
+        Abort = 2,
+        Starting = 3,
+        Stopping = 4
+    }
+}
