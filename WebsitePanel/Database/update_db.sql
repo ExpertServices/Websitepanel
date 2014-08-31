662c44510ae0a846a34b9d046b98170ea09fee99
robvde <robvde@localhost>
2014-08-31 09:28:14 +0800

    Partial Check on Hyper-V 2012 R2 VPS provider

--- WebsitePanel/Database/update_db.sql
+++ WebsitePanel/Database/update_db.sql
@@ -4463,3 +4463,12 @@ BEGIN
 UPDATE [dbo].[ServiceItemTypes] SET [Suspendable] = 1 WHERE [ItemTypeID] = 38 AND [GroupID] = 23
 END
 GO
+
+
+-- Hyper-V 2012 R2
+IF NOT EXISTS (SELECT * FROM [dbo].[Providers] WHERE [ProviderName] = 'Microsoft Hyper-V 2012 R2')
+BEGIN
+INSERT [dbo].[Providers] ([ProviderID], [GroupID], [ProviderName], [DisplayName], [ProviderType], [EditorControl], [DisableAutoDiscovery]) VALUES (350, 30, N'HyperV2012R2', N'Microsoft Hyper-V 2012 R2', N'WebsitePanel.Providers.Virtualization.HyperV2012R2, WebsitePanel.Providers.Virtualization.HyperV2012R2', N'HyperV', 1)
+END
+GO
+
