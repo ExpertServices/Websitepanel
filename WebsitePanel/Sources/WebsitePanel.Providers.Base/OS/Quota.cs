ae7712b9a3f20efc4db048123b94b4168c3ff09b
Virtuworks <Virtuworks@localhost>
2014-12-17 12:05:23 -0500

    Merge

---
+++ WebsitePanel/Sources/WebsitePanel.Providers.Base/OS/Quota.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebsitePanel.Providers.OS
+{
+    public class Quota
+    {
+        #region Fields
+
+        private int _Size;
+        private QuotaType _QuotaType;
+        private int _Usage;
+
+        #endregion
+
+        #region Properties
+
+        public int Size
+        {
+            get { return _Size; }
+            set { _Size = value; }
+        }
+
+        public QuotaType QuotaType
+        {
+            get { return _QuotaType; }
+            set { _QuotaType = value; }
+        }
+
+        public int Usage
+        {
+            get { return _Usage; }
+            set { _Usage = value; }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public Quota()
+        {
+            _Size = -1;
+            _QuotaType = QuotaType.Soft;
+            _Usage = -1;
+        }
+
+        #endregion
+    }
+}
