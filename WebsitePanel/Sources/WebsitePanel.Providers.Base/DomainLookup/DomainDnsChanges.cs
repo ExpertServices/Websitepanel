edf0d921e5875e05ae988b58dcf0fdcd6c036832
vfedosevich <vfedosevich@localhost>
2014-12-17 05:53:53 -0800

    Managing remote application users

---
+++ WebsitePanel/Sources/WebsitePanel.Providers.Base/DomainLookup/DomainDnsChanges.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebsitePanel.Providers.DomainLookup
+{
+    public class DomainDnsChanges
+    {
+        public string DomainName { get; set; }
+
+        public List<DnsRecordInfoChange> DnsChanges { get; set; }
+
+        public DomainDnsChanges()
+        {
+            DnsChanges = new List<DnsRecordInfoChange>();
+        }
+    }
+}
---
+++ WebsitePanel/Sources/WebsitePanel.WebPortal/DesktopModules/WebsitePanel/ProviderControls/IceWarp_EditForwarding.ascx
@@ -0,0 +1,1 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="IceWarp_EditForwarding.ascx.cs" Inherits="WebsitePanel.Portal.ProviderControls.IceWarp_EditForwarding" %>
