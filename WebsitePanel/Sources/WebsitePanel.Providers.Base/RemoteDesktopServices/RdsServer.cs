0352ec5ed14da52ad432ff40f10acca21d42f16b
a.skorina <a.skorina@itransition.com>
2014-12-18 17:50:06 +0300

    scheduler installer fix

---
+++ WebsitePanel/Sources/WebsitePanel.Providers.Base/RemoteDesktopServices/RdsServer.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+
+namespace WebsitePanel.Providers.RemoteDesktopServices
+{
+    public class RdsServer
+    {
+        public int Id { get; set; }
+        public int? ItemId { get; set; }
+        public string Name
+        {
+            get
+            {
+                return string.IsNullOrEmpty(FqdName) ? string.Empty : FqdName.Split('.')[0];
+            }
+        }
+        public string FqdName { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+        public string ItemName { get; set; }
+        public int? RdsCollectionId { get; set; }
+        public bool ConnectionEnabled { get; set; }
+    }
+}
