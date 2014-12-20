cba8acccfad9bd8e000d9d5c6702487de374fcfb
Olov Karlsson <olov@vildmarksdata.se>
2014-12-20 18:51:25 +0100

    Do not allow Mail or Organizations when creating new space with a IDN domain

---
+++ WebsitePanel/Sources/WebsitePanel.Providers.Base/OS/SystemFilesPaged.cs
@@ -0,0 +1,20 @@
+﻿namespace WebsitePanel.Providers.OS
+{
+    public class SystemFilesPaged
+    {
+        int recordsCount;
+        SystemFile[] pageItems;
+
+        public int RecordsCount
+        {
+            get { return this.recordsCount; }
+            set { this.recordsCount = value; }
+        }
+
+        public SystemFile[] PageItems
+        {
+            get { return this.pageItems; }
+            set { this.pageItems = value; }
+        }
+    }
+}
---
+++ WebsitePanel/Sources/WebsitePanel.WebPortal/DesktopModules/WebsitePanel/SettingsDomainLookupLetter.ascx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebsitePanel.EnterpriseServer;
+
+namespace WebsitePanel.Portal
+{
+    public partial class SettingsDomainLookupLetter : WebsitePanelControlBase, IUserSettingsEditorControl
+    {
+        public void BindSettings(UserSettings settings)
+        {
+            txtFrom.Text = settings["From"];
+            txtCC.Text = settings["CC"];
+            txtSubject.Text = settings["Subject"];
+            Utils.SelectListItem(ddlPriority, settings["Priority"]);
+            txtHtmlBody.Text = settings["HtmlBody"];
+            txtTextBody.Text = settings["TextBody"];
+
+            txtNoChangesHtmlBody.Text = settings["NoChangesHtmlBody"];
+            txtNoChangesTextBody.Text = settings["NoChangesTextBody"];
+        }
+
+        public void SaveSettings(UserSettings settings)
+        {
+            settings["From"] = txtFrom.Text;
+            settings["CC"] = txtCC.Text;
+            settings["Subject"] = txtSubject.Text;
+            settings["Priority"] = ddlPriority.SelectedValue;
+            settings["HtmlBody"] = txtHtmlBody.Text;
+            settings["TextBody"] = txtTextBody.Text;
+
+            settings["NoChangesHtmlBody"] = txtNoChangesHtmlBody.Text;
+            settings["NoChangesTextBody"] = txtNoChangesTextBody.Text;
+        }
+    }
+}
