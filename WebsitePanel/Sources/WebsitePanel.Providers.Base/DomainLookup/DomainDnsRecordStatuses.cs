fb1b31cd7b169cbcccff44250626d4d32763cd69
dev_amdtel <dev_amdtel@localhost>
2015-01-05 01:20:54 +0400

    fix Calculate Spaces Disk Space scheduled Task. part 2

---
+++ WebsitePanel/Sources/WebsitePanel.Providers.Base/DomainLookup/DomainDnsRecordStatuses.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebsitePanel.Providers.DomainLookup
+{
+    public enum DomainDnsRecordStatuses
+    {
+        NotChanged,
+        Removed,
+        Added,
+        Updated
+    }
+}
