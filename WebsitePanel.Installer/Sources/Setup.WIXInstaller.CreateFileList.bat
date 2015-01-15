f88a387996eb7a2a2e90d90e4a0e8bef02a7827a
dev_amdtel <dev_amdtel@localhost>
2015-01-15 10:55:13 +0400

    WIXInstaller. part 4

--- WebsitePanel.Installer/Sources/Setup.WIXInstaller.CreateFileList.bat
+++ WebsitePanel.Installer/Sources/Setup.WIXInstaller.CreateFileList.bat
@@ -1,6 +1,6 @@
 
-"%WIX%\bin\heat.exe" dir ..\..\WebsitePanel\Build\Release\EnterpriseServer -o Setup.WIXInstaller\EnterpriseServerFiles.wxs -gg  -sreg -var wix.BUILDESPATH -cg EnterpriseServerFiles
+"%WIX%\bin\heat.exe" dir ..\..\WebsitePanel\Build\Release\EnterpriseServer -o Setup.WIXInstaller\EnterpriseServerFiles.wxs -gg -sreg -srd -var wix.BUILDESPATH -cg EnterpriseServerFiles -dr INSTALLENTERPRISESERVERFOLDER
 
-"%WIX%\bin\heat.exe" dir ..\..\WebsitePanel\Build\Release\Server -o Setup.WIXInstaller\ServerFiles.wxs -gg -sreg -var wix.BUILDSPATH -cg ServerFiles
+"%WIX%\bin\heat.exe" dir ..\..\WebsitePanel\Build\Release\Server -o Setup.WIXInstaller\ServerFiles.wxs -gg -sreg -srd -var wix.BUILDSPATH -cg ServerFiles -dr INSTALLSERVERFOLDER
 
-"%WIX%\bin\heat.exe" dir ..\..\WebsitePanel\Build\Release\Portal -o Setup.WIXInstaller\PortalFiles.wxs -gg -sreg -var wix.BUILDPPATH -cg PortalFiles
+"%WIX%\bin\heat.exe" dir ..\..\WebsitePanel\Build\Release\Portal -o Setup.WIXInstaller\PortalFiles.wxs -gg -sreg -srd -var wix.BUILDPPATH -cg PortalFiles -dr INSTALLPORTALFOLDER
