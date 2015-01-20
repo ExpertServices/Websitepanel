27b2ced36d1e2939d86169623aff0a8a629f5a4a
vfedosevich <vfedosevich@localhost>
2015-01-20 00:37:15 -0800

    RDS Collection tabbed interface

---
+++ WebsitePanel/Sources/WebsitePanel.WebPortal/DesktopModules/WebsitePanel/RDS/UserControls/RDSCollectionTabs.ascx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebsitePanel.Portal.Code.UserControls;
+
+namespace WebsitePanel.Portal.RDS.UserControls
+{
+    public partial class RdsServerTabs : WebsitePanelControlBase
+    {
+        public string SelectedTab { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            BindTabs();
+        }
+
+        private void BindTabs()
+        {
+            List<Tab> tabsList = new List<Tab>();
+            tabsList.Add(CreateTab("rds_edit_collection", "Tab.RdsServers"));
+            tabsList.Add(CreateTab("rds_collection_edit_apps", "Tab.RdsApplications"));
+            tabsList.Add(CreateTab("rds_collection_edit_users", "Tab.RdsUsers"));
+            
+            int idx = 0;
+
+            foreach (Tab tab in tabsList)
+            {
+                if (String.Compare(tab.Id, SelectedTab, true) == 0)
+                {
+                    break;
+                }
+
+                idx++;
+            }
+
+            rdsTabs.SelectedIndex = idx;
+            rdsTabs.DataSource = tabsList;
+            rdsTabs.DataBind();
+        }
+
+        private Tab CreateTab(string id, string text)
+        {
+            return new Tab(id, GetLocalizedString(text),
+                HostModule.EditUrl("AccountID", PanelRequest.AccountID.ToString(), id,
+                "SpaceID=" + PanelSecurity.PackageId.ToString(),
+                "ItemID=" + PanelRequest.ItemID.ToString(), "CollectionID=" + PanelRequest.CollectionID));
+        }
+    }
+}
