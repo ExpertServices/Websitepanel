629b7363843e7b21e5b15a3d60440b3446a46ebc
Christiaan Swiers <c.swiers@ncaonline.nl>
2015-01-28 16:16:07 +0100

    Added user session to solve dataset issue when user is changed

---
+++ WebsitePanel/Sources/WebsitePanel.WebDav.Core/Config/Entities/OfficeOnlineCollection.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using WebsitePanel.WebDavPortal.WebConfigSections;
+
+namespace WebsitePanel.WebDav.Core.Config.Entities
+{
+    public class OfficeOnlineCollection : AbstractConfigCollection, IReadOnlyCollection<OfficeOnlineElement>
+    {
+        private readonly IList<OfficeOnlineElement> _officeExtensions;
+
+        public OfficeOnlineCollection()
+        {
+            IsEnabled = ConfigSection.OfficeOnline.IsEnabled;
+            Url = ConfigSection.OfficeOnline.Url;
+            _officeExtensions = ConfigSection.OfficeOnline.Cast<OfficeOnlineElement>().ToList();
+        }
+
+        public bool IsEnabled { get; private set; }
+        public string Url { get; private set; }
+
+        public IEnumerator<OfficeOnlineElement> GetEnumerator()
+        {
+            return _officeExtensions.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public int Count
+        {
+            get { return _officeExtensions.Count; }
+        }
+
+        public bool Contains(string extension)
+        {
+            return _officeExtensions.Any(x=>x.Extension == extension);
+        }
+    }
+}
---
+++ WebsitePanel/Sources/WebsitePanel.WebPortal/DesktopModules/WebsitePanel/SpaceMenu.ascx
@@ -0,0 +1,9 @@
+<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="SpaceMenu.ascx.cs" Inherits="WebsitePanel.Portal.SpaceMenu" %>
+
+<asp:Localize ID="locMenuTitle" runat="server" meta:resourcekey="locMenuTitle" Visible="false"></asp:Localize>
+
+<asp:Menu ID="menu" runat="server"
+    Orientation="Horizontal"
+    EnableViewState="false"
+    CssSelectorClass="TopMenu" >
+</asp:Menu>
