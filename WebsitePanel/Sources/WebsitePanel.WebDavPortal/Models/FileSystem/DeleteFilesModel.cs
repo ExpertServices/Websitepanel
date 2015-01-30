9e755d81edf875fbc85b29315af5e23227610fd4
JoeDavis <JoeDavis@localhost>
2015-01-30 13:37:22 -0600

    Fix LastName mapping and bug where forwarding was enabled without forwarding address in hMailServer5

---
+++ WebsitePanel/Sources/WebsitePanel.WebDavPortal/Models/FileSystem/DeleteFilesModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using WebsitePanel.WebDavPortal.Models.Common;
+
+namespace WebsitePanel.WebDavPortal.Models.FileSystem
+{
+    public class DeleteFilesModel : BaseModel
+    {
+        public DeleteFilesModel()
+        {
+            DeletedFiles = new List<string>();
+        }
+
+        public List<string> DeletedFiles { get; set; }
+    }
+}
