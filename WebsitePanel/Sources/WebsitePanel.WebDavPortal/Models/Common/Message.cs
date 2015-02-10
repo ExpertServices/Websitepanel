18de53ed7ba8c111bd6ece068dfd7c4a722b9447
vfedosevich <vfedosevich@localhost>
2015-02-10 06:41:00 -0800

    Fix for Remote Application users

---
+++ WebsitePanel/Sources/WebsitePanel.WebDavPortal/Models/Common/Message.cs
@@ -0,0 +1,10 @@
+﻿using WebsitePanel.WebDavPortal.Models.Common.Enums;
+
+namespace WebsitePanel.WebDavPortal.Models.Common
+{
+    public class Message
+    {
+        public MessageType Type {get;set;}
+        public string Value { get; set; }
+    }
+}
