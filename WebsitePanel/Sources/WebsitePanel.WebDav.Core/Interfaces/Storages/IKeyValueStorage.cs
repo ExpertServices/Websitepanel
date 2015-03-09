eb4ae386262bbfa7eaa09008af5f3415c93121a9
vfedosevich <vfedosevich@localhost>
2015-03-09 06:53:35 -0700

    RDS Fixes

---
+++ WebsitePanel/Sources/WebsitePanel.WebDav.Core/Interfaces/Storages/IKeyValueStorage.cs
@@ -0,0 +1,9 @@
+﻿namespace WebsitePanel.WebDav.Core.Interfaces.Storages
+{
+    public interface IKeyValueStorage
+    {
+        TV Get<TV>(string id);
+        bool Add<TV>(string id, TV value);
+        bool Delete(string id);
+    }
+}
