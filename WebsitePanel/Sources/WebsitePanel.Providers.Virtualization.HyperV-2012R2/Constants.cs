4c599420cabffd617cbceb4d738113feb088ae11
me <me@localhost>
2015-03-22 21:18:23 +0400

    wsp-10323 Fix build settings

---
+++ WebsitePanel/Sources/WebsitePanel.Providers.Virtualization.HyperV-2012R2/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebsitePanel.Providers.Virtualization
+{
+    public static class Constants
+    {
+        public const Int64 Size1G = 0x40000000;
+        public const Int64 Size1M = 0x100000;
+    }
+}
---
+++ WebsitePanel/Sources/WebsitePanel.WebDavPortal/App_Start/Routes/AccountRouteNames.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebsitePanel.WebDavPortal.UI.Routes
+{
+    public class AccountRouteNames
+    {
+        public const string Logout = "AccountLogout";
+        public const string Login = "AccountLogin";
+    }
+}
