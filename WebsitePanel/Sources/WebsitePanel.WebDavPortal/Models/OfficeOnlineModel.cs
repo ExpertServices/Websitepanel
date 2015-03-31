7fdd411dcdd438812c746ebce5437edfff9f6282
vfedosevich <vfedosevich@localhost>
2015-03-31 01:26:36 -0700

    RDS GPO changes

---
+++ WebsitePanel/Sources/WebsitePanel.WebDavPortal/Models/OfficeOnlineModel.cs
@@ -0,0 +1,18 @@
+﻿using WebsitePanel.WebDavPortal.Models.Common;
+
+namespace WebsitePanel.WebDavPortal.Models
+{
+    public class OfficeOnlineModel : BaseModel
+    {
+        public string Url { get; set; }
+        public string FileName { get; set; }
+        public string Backurl { get; set; }
+
+        public OfficeOnlineModel(string url, string fileName, string backUrl)
+        {
+            Url = url;
+            FileName = fileName;
+            Backurl = backUrl;
+        }
+    }
+}
