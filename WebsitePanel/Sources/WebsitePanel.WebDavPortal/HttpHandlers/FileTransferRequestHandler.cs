bed06fac6e5e88d9e0042b47f5519288345e6967
robvde <robvde@localhost>
2015-04-06 18:18:29 +0800

    sharepoint 2013 remove old provider Fix up organization menu

---
+++ WebsitePanel/Sources/WebsitePanel.WebDavPortal/HttpHandlers/FileTransferRequestHandler.cs
@@ -0,0 +1,26 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.SessionState;
+using System.Web.Hosting;
+using WebsitePanel.WebDavPortal.DependencyInjection;
+using WebsitePanel.WebDavPortal.Models;
+
+namespace WebsitePanel.WebDavPortal.HttpHandlers
+{
+    public class FileTransferRequestHandler : IHttpHandler 
+    {
+        public void ProcessRequest(HttpContext context) 
+        {
+            context.Response.WriteFile(context.Request.RawUrl.TrimEnd('?'));
+            context.Response.End();
+        }
+
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+    }
+}
---
+++ WebsitePanel/Sources/WebsitePanel.WebPortal/DesktopModules/WebsitePanel/ProviderControls/SmarterMail100_EditForwarding.ascx
@@ -0,0 +1,1 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="SmarterMail100_EditForwarding.ascx.cs" Inherits="WebsitePanel.Portal.ProviderControls.SmarterMail100_EditForwarding" %>
