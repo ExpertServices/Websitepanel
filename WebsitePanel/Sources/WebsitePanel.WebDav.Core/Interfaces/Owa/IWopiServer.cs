ee0ebc76c430ca635b349df30cc3e3c870f6a9a1
Virtuworks <Virtuworks@localhost>
2015-04-07 09:32:26 -0400

    Added tag build-2.1.0.659 for changeset 5cf3e8bedc1a

---
+++ WebsitePanel/Sources/WebsitePanel.WebDav.Core/Interfaces/Owa/IWopiServer.cs
@@ -0,0 +1,12 @@
+﻿using WebsitePanel.EnterpriseServer.Base.HostedSolution;
+using WebsitePanel.WebDav.Core.Client;
+using WebsitePanel.WebDav.Core.Entities.Owa;
+
+namespace WebsitePanel.WebDav.Core.Interfaces.Owa
+{
+    public interface IWopiServer
+    {
+        CheckFileInfo GetCheckFileInfo(WebDavAccessToken token);
+        byte[] GetFileBytes(int accessTokenId);
+    }
+}
