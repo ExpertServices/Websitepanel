f64bfabf2f04be115f3c137b6d9d1924efec540b
alexY <alexY@localhost>
2015-04-27 16:43:30 +0300

    Searchbox controls replaced by JQuery autocomplete.

---
+++ WebsitePanel/Sources/WebsitePanel.WebPortal/DesktopModules/WebsitePanel/Code/Helpers/AjaxHandler.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Collections.Generic;
+using System.Data;
+using WebsitePanel.Portal;
+
+namespace WebsitePanel.WebPortal
+{
+    public class WebsitePanelAjaxHandler : IHttpHandler
+    {
+        public bool IsReusable { get { return true; } }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            String filterValue = context.Request.Params["term"];
+            String fullType = context.Request.Params["fullType"];
+            String columnType = context.Request.Params["columnType"];
+
+            if (fullType == "Spaces")
+            {
+                String strItemType = context.Request.Params["itemType"];
+                int itemType = Int32.Parse(strItemType);
+                DataSet dsObjectItems = ES.Services.Packages.SearchServiceItemsPaged(PanelSecurity.EffectiveUserId, itemType,
+                    String.Format("%{0}%", filterValue),
+                   "",0, 100);
+                DataTable dt = dsObjectItems.Tables[1];
+                List<Dictionary<string, string>> dataList = new List<Dictionary<string, string>>();
+                for (int i = 0; i < dt.Rows.Count; ++i)
+                {
+                    DataRow row = dt.Rows[i];
+                    Dictionary<string, string> obj = new Dictionary<string, string>();
+                    obj["ColumnType"] = "PackageName";
+                    obj["TextSearch"] = row["PackageName"].ToString();
+                    obj["FullType"] = "Space";
+                    dataList.Add(obj);
+                }
+
+                var jsonSerialiser = new JavaScriptSerializer();
+                var json = jsonSerialiser.Serialize(dataList);
+                context.Response.ContentType = "text/plain";
+                context.Response.Write(json);
+            }
+            else
+            {
+                DataSet dsObjectItems = ES.Services.Packages.GetSearchObject(PanelSecurity.EffectiveUserId, null,
+                    String.Format("%{0}%", filterValue),
+                   0, 0, "", 0, 100, columnType);
+                DataTable dt = dsObjectItems.Tables[2];
+                List<Dictionary<string, string>> dataList = new List<Dictionary<string, string>>();
+                for (int i = 0; i < dt.Rows.Count; ++i)
+                {
+                    DataRow row = dt.Rows[i];
+                    Dictionary<string, string> obj = new Dictionary<string, string>();
+                    obj["ColumnType"] = row["ColumnType"].ToString();
+                    obj["TextSearch"] = row["TextSearch"].ToString();
+                    obj["FullType"] = row["FullType"].ToString();
+                    dataList.Add(obj);
+                }
+
+                var jsonSerialiser = new JavaScriptSerializer();
+                var json = jsonSerialiser.Serialize(dataList);
+                context.Response.ContentType = "text/plain";
+                context.Response.Write(json);
+            }
+        }
+    }
+};
---
+++ WebsitePanel/Sources/WebsitePanel.WebPortal/DesktopModules/WebsitePanel/VPSForPC/VdcManagementNetwork.ascx
@@ -0,0 +1,59 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="VdcManagementNetwork.ascx.cs" Inherits="WebsitePanel.Portal.VPSForPC.VdcManagementNetwork" %>
+<%@ Register Src="UserControls/Menu.ascx" TagName="Menu" TagPrefix="wsp" %>
+<%@ Register Src="UserControls/Breadcrumb.ascx" TagName="Breadcrumb" TagPrefix="wsp" %>
+<%@ Register Src="../UserControls/PackageIPAddresses.ascx" TagName="PackageIPAddresses" TagPrefix="wsp" %>
+
+
+<div id="VpsContainer">
+    <div class="Module">
+
+	    <div class="Header">
+		    <wsp:Breadcrumb id="breadcrumb" runat="server" />
+	    </div>
+    	
+	    <div class="Left">
+		    <wsp:Menu id="menu" runat="server" SelectedItem="vdc_management_network" />
+	    </div>
+    	
+        <div class="Center">
+			    <div class="Title">
+				    <asp:Image ID="imgIcon" SkinID="Network48" runat="server" />
+				    <asp:Localize ID="locTitle" runat="server" meta:resourcekey="locTitle" Text="Management Network"></asp:Localize>
+			    </div>
+                <div class="FormBody">
+                    <asp:Button ID="btnAddVlan" runat="server" meta:resourcekey="btnAddVlan" Text="Add"
+                        CssClass="Button1" OnClick="btnAddVlan_Click" />
+                    <br />
+                    <asp:GridView ID="gvVlans" runat="server" AutoGenerateColumns="false" CssSelectorClass="NormalGridView"
+                        EmptyDataText="User has no VLANs" OnRowCommand="gvVlans_RowCommand">
+                        <Columns>
+                            <asp:BoundField DataField="VLanID" HeaderText="VLan" />
+                            <asp:BoundField DataField="Comment" HeaderText="Comment" ItemStyle-Wrap="true" />
+                            <asp:TemplateField>
+                                <ItemTemplate>
+                                    <asp:ImageButton ID="cmdDelete" runat="server" Text="Delete" SkinID="VpsDelete" CommandName="DeleteItem"
+                                        CommandArgument='<%# Eval("VLanID") %>' meta:resourcekey="cmdDelete" OnClientClick="return confirm('Remove this item?');" />
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                        </Columns>
+                    </asp:GridView>
+                </div>
+			    <div class="FormBody">
+
+                    
+                    <wsp:PackageIPAddresses id="packageAddresses" runat="server"
+                            Pool="VpsManagementNetwork"
+                            EditItemControl="vps_general"
+                            SpaceHomeControl="vdc_management_network"
+                            AllocateAddressesControl=""
+                            ManageAllowed="true" />
+                            
+			    </div>
+		    </div>
+		    <div class="Right">
+			    <asp:Localize ID="FormComments" runat="server" meta:resourcekey="FormComments"></asp:Localize>
+		    </div>
+	    </div>
+    	
+    </div>
+</div>
