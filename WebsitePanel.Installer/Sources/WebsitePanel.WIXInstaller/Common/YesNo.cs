902fed8b0134868c2b8e00e853da92fb719b825b
alexY <alexY@localhost>
2015-04-29 13:29:08 +0300

    Global search autocomplete should open concrete object if selected from list.

---
+++ WebsitePanel.Installer/Sources/WebsitePanel.WIXInstaller/Common/YesNo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WebsitePanel.WIXInstaller.Common
+{
+    internal struct YesNo
+    {
+        public static string Yes { get { return "1"; } }
+
+        public static string No { get { return "0"; } }
+    }
+}
