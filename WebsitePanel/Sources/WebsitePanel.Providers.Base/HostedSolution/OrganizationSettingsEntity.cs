c71fca9a8297a0ea1ed51e39722f52e9580d1d18
me <me@localhost>
2015-04-29 17:51:02 +0400

    Hyper-V bugfixing

---
+++ WebsitePanel/Sources/WebsitePanel.Providers.Base/HostedSolution/OrganizationSettingsEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace WebsitePanel.Providers.HostedSolution
+{
+    public class OrganizationSettingsEntity
+    {
+        public int ItemId { get; set; }
+        public string SettingsName { get; set; }
+        public string Xml { get; set; }
+    }
+}
