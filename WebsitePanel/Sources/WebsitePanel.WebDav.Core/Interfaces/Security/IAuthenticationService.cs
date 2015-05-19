a7b734b8b17cbeec142bd2cfb544f11da46c8533
McMak <McMak@localhost>
2015-05-19 14:15:06 +0300

    Installer: old to new update #1

---
+++ WebsitePanel/Sources/WebsitePanel.WebDav.Core/Interfaces/Security/IAuthenticationService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Security;
+using WebsitePanel.EnterpriseServer.Base.HostedSolution;
+using WebsitePanel.WebDav.Core.Security.Authentication.Principals;
+
+namespace WebsitePanel.WebDav.Core.Interfaces.Security
+{
+    public interface IAuthenticationService
+    {
+        WspPrincipal LogIn(string login, string password);
+        void CreateAuthenticationTicket(WspPrincipal principal);
+        void LogOut();
+        bool ValidateAuthenticationData(string login, string password);
+    }
+}
---
+++ WebsitePanel/Sources/WebsitePanel.WebPortal/DesktopModules/WebsitePanel/VPS/ProviderControls/HyperV2012R2_Edit.ascx
@@ -0,0 +1,4 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="HyperV2012R2_Edit.ascx.cs" Inherits="WebsitePanel.Portal.ProviderControls.HyperV2012R2_Edit" %>
+<%@ Register TagPrefix="wsp" TagName="DynamicMemory" Src="../UserControls/DynamicMemory.ascx" %>
+
+<wsp:DynamicMemory runat="server" ID="DynamicMemorySetting"></wsp:DynamicMemory>
