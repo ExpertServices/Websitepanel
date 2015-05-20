4e7b1cb83f016f81eaf9392d525262546060ec53
Virtuworks <Virtuworks@localhost>
2015-05-20 11:30:25 -0400

    Added tag build-2.1.0.731 for changeset 1df9bccc82aa

---
+++ WebsitePanel.Installer/Sources/WebsitePanel.Setup/Internal/XmlDocumentMerge.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace WebsitePanel.Setup.Internal
+{
+    public static class XmlDocumentMerge
+    {
+        const string SuccessFormat = "Success: {0}.";
+        const string ErrorFormat = "Error: {0}.";
+        const string MergeCompleted = "XmlDocumentMerge completed";
+        static XmlDocumentMerge()
+        {
+            KeyAttributes = new List<string> { "name", "id", "key" };
+        }
+        public static List<string> KeyAttributes { get; set; }
+        public static string Process(string Src, string Dst, string SaveTo = "")
+        {
+            var Result = string.Empty;
+            if (!File.Exists(Src))
+                Result = string.Format(ErrorFormat, string.Format("source document [{0}] does not exists", Src));
+            else if (!File.Exists(Dst))
+                Result = string.Format(ErrorFormat, string.Format("destination document [{0}] does not exists", Dst));
+            else
+            {
+                try
+                {
+                    var InStream = new FileStream(Src, FileMode.Open, FileAccess.Read);
+                    var OutStream = new FileStream(Dst, FileMode.Open, FileAccess.ReadWrite);
+                    Result = Process(InStream,
+                                     OutStream,
+                                     SaveTo);
+                    InStream.Close();
+                    OutStream.Flush();
+                    OutStream.Close();
+                }
+                catch (Exception ex)
+                {
+                    Result = string.Format(ErrorFormat, ex.ToString());
+                }
+            }
+            return Result;
+        }
+        public static string Process(Stream InSrc, Stream OutDst, string SaveTo = "")
+        {
+            var Result = string.Format(SuccessFormat, MergeCompleted);
+            try
+            {
+                var SrcDoc = new XmlDocument();
+                SrcDoc.Load(InSrc);
+                var DstDoc = new XmlDocument();
+                DstDoc.Load(OutDst);
+                var DstNavi = DstDoc.CreateNavigator();
+                var DstIterator = DstNavi.SelectChildren(XPathNodeType.All);
+                while (DstIterator.MoveNext())
+                    Merge(DstIterator.Current.Clone(), SrcDoc, string.Empty);
+                if (string.IsNullOrWhiteSpace(SaveTo))
+                {
+                    OutDst.SetLength(0);
+                    DstDoc.Save(OutDst);
+                }
+                else
+                    DstDoc.Save(SaveTo);
+            }
+            catch (Exception ex)
+            {
+                Result = string.Format(ErrorFormat, ex.ToString());
+            }
+            return Result;
+        }
+        private static string NodePath(string Parent, string Current)
+        {
+            var Result = string.Empty;
+            if (!string.IsNullOrWhiteSpace(Parent) && !string.IsNullOrWhiteSpace(Current))
+                Result = string.Format("{0}/{1}", Parent, Current);
+            else if (!string.IsNullOrWhiteSpace(Parent))
+                Result = Parent;
+            else if (!string.IsNullOrWhiteSpace(Current))
+                Result = Current;
+            return Result;
+        }
+        private static string NodeView(XPathNavigator Navi)
+        {
+            foreach (var Attr in KeyAttributes)
+            {
+                var Value = Navi.GetAttribute(Attr, string.Empty);
+                if (!string.IsNullOrWhiteSpace(Value))
+                    return string.Format("{0}[@{1}='{2}']", Navi.Name, Attr, Value);
+            }
+            return Navi.Name;
+        }
+        private static void Merge(XPathNavigator DstNavi, XmlDocument SrcDoc, string Parent)
+        {
+            if (DstNavi.NodeType == XPathNodeType.Element)
+            {
+                var SrcElem = SrcDoc.SelectSingleNode(NodePath(Parent, NodeView(DstNavi)));
+                if (SrcElem != null)
+                {
+                    if (DstNavi.MoveToFirstAttribute())
+                    {
+                        do
+                        {
+                            var SrcElemAttr = SrcElem.Attributes[DstNavi.LocalName];
+                            if (SrcElemAttr != null)
+                                DstNavi.SetValue(SrcElemAttr.Value);
+                        }
+                        while (DstNavi.MoveToNextAttribute());
+                        DstNavi.MoveToParent();
+                    }
+                }
+            }
+            else if (DstNavi.NodeType == XPathNodeType.Text)
+            {
+                var SrcElem = SrcDoc.SelectSingleNode(NodePath(Parent, NodeView(DstNavi)));
+                if (SrcElem != null)
+                    DstNavi.SetValue(SrcElem.InnerText);
+            }
+            var Here = NodeView(DstNavi);
+            if (DstNavi.MoveToFirstChild())
+            {
+                do
+                {
+                    Merge(DstNavi, SrcDoc, NodePath(Parent, Here));
+                }
+                while (DstNavi.MoveToNext());
+                DstNavi.MoveToParent();
+            }
+            else if (DstNavi.NodeType == XPathNodeType.Element)
+            {
+                var SrcElem = SrcDoc.SelectSingleNode(NodePath(Parent, Here));
+                if (SrcElem != null && !string.IsNullOrWhiteSpace(SrcElem.InnerXml))
+                    foreach (XmlNode Child in SrcElem.ChildNodes)
+                        DstNavi.AppendChild(Child.CloneNode(true).CreateNavigator());
+            }
+        }
+    }
+}
---
+++ WebsitePanel/Sources/WebsitePanel.WebPortal/DesktopModules/WebsitePanel/SkinControls/GlobalSearch.ascx
@@ -0,0 +1,124 @@
+<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="GlobalSearch.ascx.cs" Inherits="WebsitePanel.Portal.SkinControls.GlobalSearch" %>
+
+<style>
+    .ui-menu-item a {white-space: nowrap; }
+</style>
+
+<script type="text/javascript">
+    //<![CDATA[
+    $("#<%= tbSearch.ClientID %>").keypress(function (e) {
+        if (e.keyCode != 13) { // VK_RETURN
+            $("#<%= tbSearchText.ClientID %>").val('');
+            $("#<%= tbObjectId.ClientID %>").val('');
+            $("#<%= tbPackageId.ClientID %>").val('');
+            $("#<%= tbAccountId.ClientID %>").val('');
+        }
+    });
+
+    $(document).ready(function () {
+        $("#<%= tbSearch.ClientID %>").autocomplete({
+            zIndex: 100,
+            source: function(request, response) {
+                $.ajax({
+                    type: "post",
+                    dataType: "json",
+                    data: {
+                        term: request.term
+                    },
+                    url: "AjaxHandler.ashx",
+                    success: function(data)
+                    {
+                        response($.map(data, function (item) {
+                            return {
+                                label: item.TextSearch + " [" + item.FullTypeLocalized + "]",
+                                code: item
+                            };
+                        }));
+                    }
+                })
+            },
+            select: function (event, ui) {
+                var item = ui.item;
+                $("#<%= tbSearchColumnType.ClientID %>").val(item.code.ColumnType);
+                $("#<%= tbSearchFullType.ClientID %>").val(item.code.FullType);
+                $("#<%= tbSearchText.ClientID %>").val(item.code.TextSearch);
+                $("#<%= tbObjectId.ClientID %>").val(item.code.ItemID);
+                $("#<%= tbPackageId.ClientID %>").val(item.code.PackageID);
+                $("#<%= tbAccountId.ClientID %>").val(item.code.AccountID);
+                var $ImgBtn = $("#<%= ImageButton1.ClientID %>");
+                $ImgBtn.trigger("click");
+                $ImgBtn.attr('disabled', 'disabled');
+            }
+        });
+        if (document.referrer.search("pid=Login") > 0) {
+            $("#<%= tbSearch.ClientID %>").focus();
+        }
+
+    });//]]>
+</script>
+
+<asp:Panel runat="server" ID="updatePanelUsers" UpdateMode="Conditional" ChildrenAsTriggers="true">
+    <ContentTemplate>
+        <table cellpadding="0" cellspacing="0" align="right">
+            <tr>
+                <td align="left" class="SearchQuery">
+                    <div class="ui-widget">
+                        <asp:TextBox
+                            ID="tbSearch"
+                            runat="server"
+                            CssClass="NormalTextBox"
+                            Width="120px"
+                            style="vertical-align: middle; z-index: 100;"
+                        >
+                        </asp:TextBox>
+                        <asp:TextBox
+                            ID="tbSearchColumnType"
+                            runat="server"
+                            type="hidden"
+                        >
+                        </asp:TextBox>
+                        <asp:TextBox
+                            ID="tbSearchFullType"
+                            runat="server"
+                            type="hidden"
+                        >
+                        </asp:TextBox>
+                        <asp:TextBox
+                            ID="tbSearchText"
+                            runat="server"
+                            type="hidden"
+                        >
+                        </asp:TextBox>
+                        <asp:TextBox
+                            ID="tbObjectId"
+                            runat="server"
+                            type="hidden"
+                        >
+                        </asp:TextBox>
+                        <asp:TextBox
+                            ID="tbPackageId"
+                            runat="server"
+                            type="hidden"
+                        >
+                        </asp:TextBox>
+                        <asp:TextBox
+                            ID="tbAccountId"
+                            runat="server"
+                            type="hidden"
+                        >
+                        </asp:TextBox>
+
+                        <asp:ImageButton
+                            ID="ImageButton1"
+                            runat="server"
+                            SkinID="SearchButton"
+                            OnClick="btnSearchObject_Click"
+                            CausesValidation="false"
+                            style="vertical-align: middle;"
+                        />                 
+                    </div>
+                </td>
+            </tr>
+        </table>
+    </ContentTemplate>
+</asp:Panel>
