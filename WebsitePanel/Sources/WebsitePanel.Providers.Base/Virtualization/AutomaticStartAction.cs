e163f800f461d673e9bac67df0c5329e37496426
Alexander Trofimov <Alexander Trofimov@localhost>
2015-05-29 13:33:37 +0300

    HyperV major bugfix

---
+++ WebsitePanel/Sources/WebsitePanel.Providers.Base/Virtualization/AutomaticStartAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebsitePanel.Providers.Virtualization
+{
+    public enum AutomaticStartAction
+    {
+        Undefined = 100,
+        Nothing = 0,
+        StartIfRunning = 1,
+        Start = 2,
+    }
+}
